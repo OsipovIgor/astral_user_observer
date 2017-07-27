07767c6544420edad6580f0d2dc3927f104188fe
osipov <osipov@astralnalog.ru>
2017-07-27 17:11:47 +0300

    socket server для приема юзернеймов

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var users = [];
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  
+  var username = socket.handshake.query.username;
+  
+  if(users.indexOf(username) === -1)
+    users.push(socket.handshake.query.username);
+
+  console.log("users", users);
+  socket.on('disconnect', function() {
+    console.log("disconnected");
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3228, function(){
+  console.log('listening on *:3228');
 });
