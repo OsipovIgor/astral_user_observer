707697fc5279cca6b42a464d33496d6c1cdb5f0b
osipov <osipov@astralnalog.ru>
2017-07-28 11:25:57 +0300

    класс для управление коннектами/дисконнектами пользователей

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
+const { UserManager } = require("./managers/UserManager");
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-const users = [];
-
-function getCurrentUsers(){
-  return users.map(u => u.name);
-}
-
 io.on('connection', function(socket) {
-  
   const username = socket.handshake.query.username;
-  const index = users.findIndex(elem => elem.name === socket.username);
-
-  if (index === -1) {
-    users.push({ id: socket.id, name: socket.handshake.query.username });
-    console.log("added user");
-  }
-
-  console.log("users after connect", getCurrentUsers());
+  UserManager.connect(socket.id, username);
 
   socket.on('disconnect', function() {
-    const index = users.findIndex(elem => elem.id === socket.id);
-
-    if (index !== -1)
-      users.splice(index, 1);
-
-    console.log("users after disconect", getCurrentUsers());
-
+    UserManager.disconnect(socket.id);
   });
 });
 
